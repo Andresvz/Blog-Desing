3ada0e493f15126c8a8596d7177ef6ab65c2b71c
andres vasquez <andresvz91@gmail.com>
2015-03-30 10:41:57 -0500

    localStore y sessionStore

--- js/form.js
+++ js/form.js
@@ -5,9 +5,17 @@ var $form = $("#form"),
     $list = $("#contenido"),
     $post = $(".item").first();
 
+if(localStorage.getItem('autosave')){
+$title.val(sessionStorage.getItem('title'));
+$url.val(sesisonStorage.getItem('url'));
 
+}var id = setInterval(function(){
+  sessionStorage.setItem('title', $title.val());
+  sessionStorage.setItem('url', $url.val());
+},1000);
 function mostrarForm(){
   $form.slideToggle();
+  $list.slideToggle();
 
   return false;
 }
@@ -16,10 +24,16 @@ function agregarPost(){
       title = $title.val(),
       $clone = $post.clone();  
   
-      $clone.find('.titulo_item a').text(title).attr('href',url);
+      $clone.find('.titulo_item a')
+      .text(title)
+      .attr('href',url);
+      
       $clone.hide();
 
       $list.prepend($clone);
+      mostrarForm();
+      $title.val('');
+      $url.val('');
 
       $clone.fadeIn();
   return false;
